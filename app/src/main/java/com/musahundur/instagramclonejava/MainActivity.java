3c5af9f12aac0f231ab10171328d3ef59271777a
Musa <musaabdullah.hundur@gmail.com>
2022-02-02 16:28:56 +0300

    update

--- app/src/main/java/com/musahundur/instagramclonejava/MainActivity.java
+++ app/src/main/java/com/musahundur/instagramclonejava/MainActivity.java
@@ -1,16 +1,61 @@
 package com.musahundur.instagramclonejava;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.musahundur.instagramclonejava.databinding.ActivityMainBinding;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
-        
+        auth = FirebaseAuth.getInstance();
+    }
+
+    public void signInClicked (View view){
+
+    }
+
+    public void signUpClicked(View view){
+        String email = binding.mailText.getText().toString();
+        String password = binding.passwordText.getText().toString();
+
+        if (email.equals("") || password.equals("")){
+            Toast.makeText(this, "Enter email and password", Toast.LENGTH_LONG).show();
+        }else{
+            auth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Intent intent = new Intent(MainActivity.this, FeedActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this,e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
     }
 }
