a33dc4b848ffe99b9ab706e2c5c2bde6615333f7
Musa <musaabdullah.hundur@gmail.com>
2022-02-02 17:12:00 +0300

    update

--- app/src/main/java/com/musahundur/instagramclonejava/FeedActivity.java
+++ app/src/main/java/com/musahundur/instagramclonejava/FeedActivity.java
@@ -1,14 +1,51 @@
 package com.musahundur.instagramclonejava;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FeedActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+
+        auth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.option_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.add_post){
+            //Go Upload Activity
+            Intent intentAddPost = new Intent(FeedActivity.this, UploadActivity.class);
+            startActivity(intentAddPost);
+        } else if(item.getItemId() == R.id.signout){
+            auth.signOut();
+            //Sign Out
+            Intent intentSignOut = new Intent(FeedActivity.this, MainActivity.class);
+            startActivity(intentSignOut);
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/musahundur/instagramclonejava/MainActivity.java
+++ app/src/main/java/com/musahundur/instagramclonejava/MainActivity.java
@@ -12,6 +12,7 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.musahundur.instagramclonejava.databinding.ActivityMainBinding;
 
 import java.util.zip.Inflater;
@@ -29,9 +30,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(view);
 
         auth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = auth.getCurrentUser();
+
+        if (user != null){
+            Intent intent = new Intent(MainActivity.this, FeedActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 
     public void signInClicked (View view){
+        String email = binding.mailText.getText().toString();
+        String password = binding.passwordText.getText().toString();
+
+        if (email.equals("")||password.equals("")){
+            Toast.makeText(MainActivity.this, "Enter email and password", Toast.LENGTH_LONG).show();
+        }else{
+            auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Intent intent = new Intent(MainActivity.this, FeedActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this,e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
 
     }
 
